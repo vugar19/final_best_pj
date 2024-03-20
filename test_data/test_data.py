17b91738b2d93b2519f3b40d6de27cbac3c6c780
User <ozmiz2001@gmail.com>
2024-03-20 23:59:32 +0200

    new req.txt file

---
+++ test_data/test_data.py
@@ -0,0 +1,6 @@
+from selenium.webdriver.common.by import By
+login = (By.XPATH,"//*[@id='login']")
+login_btn = (By.XPATH,"//*[@btn']")
+code = (By.XPATH,"//*[@id='code']")
+code_btn = (By.XPATH,"//*[@id='enter']")
+phone = "9720546215487"
