5c1411d156ff62f81703715922edbaf330d0642b
User <ozmiz2001@gmail.com>
2024-04-02 03:08:16 +0300

    small changes

--- test_data/test_data.py
+++ test_data/test_data.py
@@ -5,3 +5,21 @@ code = (By.XPATH,"//*[@id='root']/div[1]/div[2]/div/span/form/div[1]/div[1]/span
 code_btn = (By.XPATH,"//*[@id='root']/div[1]/div[2]/div/span/form/input")
 phone = "9721320207423"
 
+xpath = (By.XPATH,"//*[@id='root']")
+logout = (By.XPATH,'//*[@id="root"]/div[1]/div[2]/div/nav/div[1]/div/span[2]/a')
+coupon_btn = (By.XPATH,'//*[@id="root"]/div[1]/div[2]/div/nav/div[2]/a[4]')
+drop_down_manu = (By.XPATH,'//*[@id="root"]/div[1]/div[2]/main/div[2]/div/div[1]/div')
+add_btn = (By.XPATH,'//*[@id="root"]/div[1]/div[2]/main/div[2]/div/div[1]/div[2]/div/div[1]')
+add_form_name = (By.XPATH,'//*[@id="root"]/div[1]/div[4]/div/div/form/div[1]/div[1]/span/div/input')
+add_form_code = (By.XPATH,'//*[@id="root"]/div[1]/div[4]/div/div/form/div[1]/div[2]/span/div/input')
+add_form_min_cart_val = (By.XPATH,'//*[@id="root"]/div[1]/div[4]/div/div/form/div[1]/div[3]/span/div/input')
+add_form_start_date = (By.XPATH,'//*[@id="root"]/div[1]/div[4]/div/div/form/div[1]/div[5]/div[1]/div[1]/span/div/input')
+add_form_end_date = (By.XPATH,'//*[@id="root"]/div[1]/div[4]/div/div/form/div[1]/div[5]/div[1]/div[2]/span/div/input')
+add_form_discount_ammount = (By.XPATH,'//*[@id="root"]/div[1]/div[4]/div/div/form/div[1]/div[6]/div[1]/div[1]/span/div/input')
+add_form_type = (By.XPATH,'//*[@id="root"]/div[1]/div[4]/div/div/form/div[1]/div[6]/div[1]/div[2]/div[1]/span[1]/div/input')
+add_form_types_shekel = (By.XPATH,'//*[@id="root"]/div[1]/div[4]/div/div/form/div[1]/div[6]/div[1]/div[2]/div[1]/span[1]/div[2]/div/div[1]')
+add_form_types_precen = (By.XPATH,'//*[@id="root"]/div[1]/div[4]/div/div/form/div[1]/div[6]/div[1]/div[2]/div[1]/span[1]/div[2]/div/div[2]')
+
+final_add_btn = (By.XPATH,'//*[@id="root"]/div[1]/div[4]/div/div/form/input')
+
+error_message = (By.XPATH,'//*[@class="form_note"]')
--- tests/tests.py
+++ tests/tests.py
@@ -1,14 +1,136 @@
 import time
 
+import pytest
 
-def test_LOGIN(driver):
-    time.sleep(10)
+from common.actions import Actions
+from test_data import test_data as TD
+from selenium.webdriver.common.action_chains import ActionChains
+@pytest.mark.skip
+def test_26_1_1(driver):
+    action = Actions(driver)
+    driver.implicitly_wait(5)
+    order = action.find_element(TD.xpath)
+    order.click()
+    time.sleep(3)
+    assert driver.current_url == "http://test-admin-env.eba-fnn924ys.eu-west-1.elasticbeanstalk.com/#/orders"
+
+@pytest.mark.skip
+def test_26_1_2(driver):
+    action = ActionChains(driver)
+    actions = Actions(driver)
+    driver.implicitly_wait(5)
+    order = actions.find_element(TD.logout)
+    order_hover = action.move_to_element(order)
+    order_hover.click()
+    time.sleep(3)
+    assert driver.current_url == "http://test-admin-env.eba-fnn924ys.eu-west-1.elasticbeanstalk.com/#/"
+
+
+@pytest.mark.parametrize("name_id,name_value,code_id,code_value,min_cart_id,min_cart_value,start_date_id ,start_date_value, end_date_id,end_date_value, discount_amount_id,discount_amount_value,discount_type_id,validation_message",[
+                             (TD.add_form_name,"",TD.add_form_code,'12345',TD.add_form_min_cart_val,'100',TD.add_form_start_date,'15/01/2024',TD.add_form_end_date,'26/08/24',TD.add_form_discount_ammount,'30',TD.add_form_types_shekel,TD.add_form_name),
+                             (TD.add_form_name,"hello_world",TD.add_form_code,'',TD.add_form_min_cart_val,'100',TD.add_form_start_date,'15/01/2024',TD.add_form_end_date,'26/08/24',TD.add_form_discount_ammount,'30',TD.add_form_types_shekel,TD.add_form_code),
+                             (TD.add_form_name,"hello_world",TD.add_form_code,'12345',TD.add_form_min_cart_val,'',TD.add_form_start_date,'15/01/2024',TD.add_form_end_date,'26/08/24',TD.add_form_discount_ammount,'30',TD.add_form_types_shekel,TD.add_form_min_cart_val),
+                             (TD.add_form_name,"hello_world",TD.add_form_code,'12345',TD.add_form_min_cart_val,'100',TD.add_form_start_date,'',TD.add_form_end_date,'26/08/24',TD.add_form_discount_ammount,'30',TD.add_form_types_shekel,TD.add_form_start_date),
+                             (TD.add_form_name,"hello_world",TD.add_form_code,'12345',TD.add_form_min_cart_val,'100',TD.add_form_start_date,'15/01/2024',TD.add_form_end_date,'',TD.add_form_discount_ammount,'30',TD.add_form_types_precen,TD.add_form_end_date),
+                             (TD.add_form_name,"hello_world",TD.add_form_code,'12345',TD.add_form_min_cart_val,'100',TD.add_form_start_date,'15/01/2024',TD.add_form_end_date,'26/08/24',TD.add_form_discount_ammount,'',TD.add_form_types_precen,TD.add_form_discount_ammount),
+                             (TD.add_form_name,"hello_world",TD.add_form_code,'12345',TD.add_form_min_cart_val,'100',TD.add_form_start_date,'15/01/2024',TD.add_form_end_date,'26/08/24',TD.add_form_discount_ammount,'30','',TD.add_form_types_precen),
+                                     ])
+def test_4_2_invalid(driver,name_id,name_value,code_id,code_value,min_cart_id,min_cart_value,start_date_id ,start_date_value, end_date_id,end_date_value, discount_amount_id,discount_amount_value,discount_type_id,validation_message):
+    actions = Actions(driver)
+    driver.implicitly_wait(5)
+    coupons = actions.find_element(TD.coupon_btn)
+    coupons.click()
+    time.sleep(3)
+
+    menu = actions.find_element(TD.drop_down_manu)
+    menu.click()
+
+    add_btn = actions.find_element(TD.add_btn)
+    add_btn.click()
+
+    name_field = actions.find_element(name_id)
+    name_field.send_keys(name_value)
+
+    code_field = actions.find_element(code_id)
+    code_field.send_keys(code_value)
+
+    min_cart_field = actions.find_element(min_cart_id)
+    min_cart_field.send_keys(min_cart_value)
+
+    start_date = actions.find_element(start_date_id)
+    start_date.send_keys(start_date_value)
+
+    end_date = actions.find_element(end_date_id)
+    end_date.send_keys(end_date_value)
+
+    discount_amount = actions.find_element(discount_amount_id)
+    discount_amount.send_keys(discount_amount_value)
+
+    discount_tyoe_manu = actions.find_element(TD.add_form_type)
+    discount_tyoe_manu.click()
+
+    discount_type = actions.find_element(discount_type_id)
+    discount_type.click()
+
+    final_add_button = actions.find_element(TD.final_add_btn)
+    final_add_button.click()
+
+    validation = actions.find_element(validation_message)
+    validation_text = validation.get_attribute('validationMessage')
+
+    assert validation_text == 'Please fill in this field.'
 
 
 
 
+@pytest.mark.parametrize("name_id,name_value,code_id,code_value,min_cart_id,min_cart_value,start_date_id ,start_date_value, end_date_id,end_date_value, discount_amount_id,discount_amount_value,discount_type_id",[
+                             (TD.add_form_name,"hello",TD.add_form_code,'12345',TD.add_form_min_cart_val,'100',TD.add_form_start_date,'15/01/2024',TD.add_form_end_date,'26/08/24',TD.add_form_discount_ammount,'30',TD.add_form_types_shekel)])
+def test_4_2_valid(driver,name_id,name_value,code_id,code_value,min_cart_id,min_cart_value,start_date_id ,start_date_value, end_date_id,end_date_value, discount_amount_id,discount_amount_value,discount_type_id):
+    actions = Actions(driver)
+    driver.implicitly_wait(5)
+    coupons = actions.find_element(TD.coupon_btn)
+    coupons.click()
+    time.sleep(3)
+
+    menu = actions.find_element(TD.drop_down_manu)
+    menu.click()
+
+    add_btn = actions.find_element(TD.add_btn)
+    add_btn.click()
+
+    name_field = actions.find_element(name_id)
+    name_field.send_keys(name_value)
+
+    code_field = actions.find_element(code_id)
+    code_field.send_keys(code_value)
+
+    min_cart_field = actions.find_element(min_cart_id)
+    min_cart_field.send_keys(min_cart_value)
+
+    start_date = actions.find_element(start_date_id)
+    start_date.send_keys(start_date_value)
+
+    end_date = actions.find_element(end_date_id)
+    end_date.send_keys(end_date_value)
+
+    discount_amount = actions.find_element(discount_amount_id)
+    discount_amount.send_keys(discount_amount_value)
+
+    discount_tyoe_manu = actions.find_element(TD.add_form_type)
+    discount_tyoe_manu.click()
+
+    discount_type = actions.find_element(discount_type_id)
+    discount_type.click()
+
+    final_add_button = actions.find_element(TD.final_add_btn)
+    final_add_button.click()
+
+    sucsess_mess = actions.find_element(TD. success_mess).get_attribute('innerText')
 
 
+
+
+    assert sucsess_mess =
 
 
 
